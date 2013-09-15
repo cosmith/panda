a2d2145eb398099b02e50ca915c9cfdde083dec3
Corentin Smith <corentin.smith@gmail.com>
2013-09-15 11:05:12 -0400

    Add a 'type' field to nodes

--- nodes.js
+++ nodes.js
@@ -3,6 +3,7 @@ module.exports = {
     // Our root node, containing all the representation of the program
     Nodes: function (nodes, loc) {
         var self = this;
+        self.type = "root";
         self.nodes = nodes;
         self.loc = loc;
 
@@ -17,32 +18,38 @@ module.exports = {
 
     // Literal nodes that translate directly to javascript
     NumberNode: function (value, loc) {
+        this.type = "number";
         this.value = value;
         this.loc = loc;
     },
 
     StringNode: function (value, loc) {
+        this.type = "string";
         this.value = value;
         this.loc = loc;
     },
 
     TrueNode: function (loc) {
+        this.type = "boolean";
         this.value = true;
         this.loc = loc;
     },
 
     FalseNode: function (loc) {
+        this.type = "boolean";
         this.value = false;
         this.loc = loc;
     },
 
     NoneNode: function (loc) {
+        this.type = "none";
         this.value = null;
         this.loc = loc;
     },
 
     // method call
     CallNode: function (receiver, method, argument, loc) {
+        this.type = "call";
         this.receiver = receiver;
         this.method = method;
         this.argument = argument;
@@ -51,11 +58,13 @@ module.exports = {
 
     // local variables
     GetLocalNode: function (name, loc) {
+        this.type = "getlocal";
         this.name = name;
         this.loc = loc;
     },
 
     SetLocalNode: function (name, value, loc) {
+        this.type = "setlocal";
         this.name = name;
         this.value = value;
         this.loc = loc;
@@ -63,6 +72,7 @@ module.exports = {
 
     // function definition
     DefNode: function (name, params, body, loc) {
+        this.type = "def";
         this.name = name;
         this.params = params;
         this.body = body;
@@ -71,6 +81,7 @@ module.exports = {
 
     // if
     IfNode: function (condition, body, loc) {
+        this.type = "if";
         this.condition = condition;
         this.body = body;
         this.loc = loc;
