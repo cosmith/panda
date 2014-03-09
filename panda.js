803e02e3db915e0dcace492f26242967d9c86ed6
Corentin Smith <corentin.smith@gmail.com>
2014-03-09 16:20:02 +0100

    Add REPL prototype

--- panda.js
+++ panda.js
@@ -4,6 +4,7 @@ var fs = require('fs'); // file system
 var Panda = require('./src/panda');
 var helpers = require('./src/helpers');
 var path = "";
+var repl = require('./lib/repl');
 
 var program = require('commander');
 
@@ -17,56 +18,57 @@ program
     .option('-c, --compile', 'write compiled code to JS file')
     .parse(process.argv);
 
-// we need an input file to proceed
+// show REPL if no input file
 if (program.args.length === 0) {
-    program.outputHelp();
-    process.exit(1);
+    var repl = new repl.Repl();
+    repl.start();
 }
+else {
+    path = program.args[0];
 
-path = program.args[0];
+    fs.readFile(path, 'utf-8', function (err, data) {
+        if (err) throw err;
 
-fs.readFile(path, 'utf-8', function (err, data) {
-    if (err) throw err;
+        var tokens, ast, compiled, filename;
 
-    var tokens, ast, compiled, filename;
+        if (program.tokens || program.verbose) {
+            tokens = Panda.tokenize(data);
+            helpers.printTokens(tokens);
 
-    if (program.tokens || program.verbose) {
-        tokens = Panda.tokenize(data);
-        helpers.printTokens(tokens);
-
-        console.log("\n======\n");
-    }
-
-    if (program.ast || program.verbose) {
-        tokens = Panda.tokenize(data);
-        ast = Panda.parse(tokens);
-        helpers.printAst(ast);
-
-        console.log("\n======\n");
-    }
-
-    compiled = Panda.compile(data);
-
-    if (program.javascript || program.verbose) {
-        console.log("\n\nOutput");
-        console.log("======\n");
-
-        console.log(compiled);
-
-        console.log("\n======\n");
-    }
-
-    if (program.compile) {
-        filename = program.args[1];
-        if (!filename) {
-            filename = path.slice(0, path.length - 3) + ".js";
+            console.log("\n======\n");
         }
-        
-        fs.writeFile(filename, compiled, function (err, data) {
-            if (err) throw err;
-        });
-    }
-    else {
-        eval(compiled);
-    }
-});
+
+        if (program.ast || program.verbose) {
+            tokens = Panda.tokenize(data);
+            ast = Panda.parse(tokens);
+            helpers.printAst(ast);
+
+            console.log("\n======\n");
+        }
+
+        compiled = Panda.compile(data);
+
+        if (program.javascript || program.verbose) {
+            console.log("\n\nOutput");
+            console.log("======\n");
+
+            console.log(compiled);
+
+            console.log("\n======\n");
+        }
+
+        if (program.compile) {
+            filename = program.args[1];
+            if (!filename) {
+                filename = path.slice(0, path.length - 3) + ".js";
+            }
+            
+            fs.writeFile(filename, compiled, function (err, data) {
+                if (err) throw err;
+            });
+        }
+        else {
+            eval(compiled);
+        }
+    });
+}
